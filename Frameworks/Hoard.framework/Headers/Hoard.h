8970ae01844e9575ba77cf4a03da8f7392f7da76
Ben Gottlieb <ben@standalone.com>
2018-05-18 22:26:36 -0500

    initial commit

---
+++ Frameworks/Hoard.framework/Headers/Hoard.h
@@ -0,0 +1,19 @@
+//
+//  Hoard.h
+//  Hoard
+//
+//  Created by Ben Gottlieb on 2/9/15.
+//  Copyright (c) 2015 Stand Alone, inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Hoard.
+FOUNDATION_EXPORT double HoardVersionNumber;
+
+//! Project version string for Hoard.
+FOUNDATION_EXPORT const unsigned char HoardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Hoard/PublicHeader.h>
+
+
